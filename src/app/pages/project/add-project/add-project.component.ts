b2e75eb10b11900d49fb941df44d60a5c927ce6a
ajith.techzar@gmail.com <ajith.techzar@gmail.com>
2019-09-18 10:00:53 +0530

    sidemenu location & camera done

--- src/app/pages/project/add-project/add-project.component.ts
+++ src/app/pages/project/add-project/add-project.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProjectService } from 'src/app/@core/services/project.service';
@@ -9,15 +8,6 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Country } from 'src/app/@core/models/country.model';
 import { CommonService } from 'src/app/@core/services/common.service';
-=======
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { ProjectService } from "src/app/@core/services/project.service";
-import { ToastrService } from "ngx-toastr";
-import { environment } from "src/environments/environment";
-import { ValidEmail } from "src/app/@core/validators/valid-email.validators";
-import { Router } from "@angular/router";
->>>>>>> dd42803eb9d87576927e58c7d9649e6841abfded
 
 @Component({
     selector: 'app-add-project',
@@ -32,7 +22,6 @@ export class AddProjectComponent implements OnInit {
     webUser: any;
     mobileUser: any;
     validator = environment.validators;
-<<<<<<< HEAD
     countryList: Observable<Country[]>;
     statusData = [ {id: 0, name: 'Inprogress'}, {id: 1, name: 'Completed'}];
     fieldEnvData = [
@@ -84,86 +73,6 @@ export class AddProjectComponent implements OnInit {
             // client_phone: ["", [Validators.required,
             //             Validators.minLength(this.validator.phone.min),
             //             Validators.maxLength(this.validator.phone.max)]],
-=======
-    statusData = [{ id: 1, name: "Inprogress" }, { id: 0, name: "Completed" }];
-    submitted = false;
-
-    constructor(
-        private fb: FormBuilder,
-        private service: ProjectService,
-        private toastr: ToastrService,
-        private router: Router
-    ) {}
-
-    ngOnInit() {
-        this.addproductForm = this.fb.group({
-            name: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            address: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            city: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            state: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            country: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            client_name: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.name.min),
-                    Validators.maxLength(this.validator.name.max)
-                ]
-            ],
-            client_email: [
-                "",
-                [
-                    Validators.required,
-                    ValidEmail,
-                    Validators.minLength(this.validator.email.min),
-                    Validators.maxLength(this.validator.email.max)
-                ]
-            ],
-            client_phone: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(this.validator.phone.min),
-                    Validators.maxLength(this.validator.phone.max)
-                ]
-            ],
-            status: ["", [Validators.required]]
->>>>>>> dd42803eb9d87576927e58c7d9649e6841abfded
         });
         // this.service.getUserType().subscribe(res => {
         //     console.log("getUserType", res);
@@ -189,7 +98,6 @@ export class AddProjectComponent implements OnInit {
 
     submit(value) {
         this.disable = false;
-<<<<<<< HEAD
         const params = {
             name: value.name,
             blockName: value.blockName,
@@ -212,45 +120,5 @@ export class AddProjectComponent implements OnInit {
           }, 1500);
 
         });
-=======
-        var params = {
-            name: value.name,
-            address: value.address,
-            city: value.city,
-            state: value.state,
-            status: value.status,
-            country: value.country,
-            clientName: value.client_name,
-            clientEmail: value.client_email,
-            clientPhone: value.client_phone
-        };
-        console.log('value', value);
-        this.service.create(params).subscribe(
-            res => {
-                this.toastr.error("", res.message);
-                this.addproductForm.reset();
-                console.log('Project create response', res.data._id);
-                // var scope = this;
-                setTimeout(() => {
-                    this.router.navigate(['/project/list']);
-                }, 1500);
-            },
-            error => {
-                this.submitted = false;
-                console.log(error);
-                if (error.errors.length > 0) {
-                    for (const fieldError of error.errors) {
-                        const check = fieldError.param;
-                        this.addproductForm
-                            .get(check)
-                            .setErrors({ customError: fieldError.msg });
-                    }
-                }
-            },
-            () => {
-                this.submitted = false;
-            }
-        );
->>>>>>> dd42803eb9d87576927e58c7d9649e6841abfded
     }
 }
